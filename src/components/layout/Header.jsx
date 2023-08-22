82ed2b88d6a57c523566c89283bd03365e5a2712
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-22 08:58:26 +0300

    active

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,6 +6,7 @@ const HeaderContainer = styled(Wrap)`
   display: flex;
   justify-content: space-between;
   gap: 15px;
+  align-items: center;
 `;
 
 const Logo = styled(Link)`
@@ -13,15 +14,28 @@ const Logo = styled(Link)`
   padding: 0.2em 0;
 `;
 
+const Nav = styled.nav``;
+
+const OneLink = styled(NavLink)`
+  font-size: 18px;
+  padding: 0.3em 0.8em;
+  border: 1px solid transparent;
+  &:hover {
+    background-color: gray;
+  }
+  &.active {
+    border: 1px solid #b3b3b3;
+  }
+`;
+
 export default function Header() {
   return (
     <HeaderContainer as={'header'}>
       <Logo to={'/'}>OurLogo</Logo>
-      <nav>
-        <a href='' className='navLink'>
-          link
-        </a>
-      </nav>
+      <Nav>
+        <OneLink to={'/'}>Home</OneLink>
+        <OneLink to={'/about'}>About Us</OneLink>
+      </Nav>
     </HeaderContainer>
   );
 }
