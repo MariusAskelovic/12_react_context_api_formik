6f62ac7e9bc9bf88622107bf766fdbeeea17bc27
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-22 15:28:19 +0300

    Register message above form

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,6 +3,8 @@ import Wrap from '../styled/Wrap.styled';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
+import { useContext, useState } from 'react';
+// import AuthContext from '../store/AuthContext';
 
 const Form = styled.form`
   width: 100%;
@@ -30,6 +32,7 @@ const ErrorMsg = styled.p`
 `;
 
 export default function RegisterPage() {
+  const [loginStatus, setLoginStatus] = useState('');
   const formik = useFormik({
     initialValues: {
       email: 'george.bluth@reqres.in',
@@ -54,7 +57,7 @@ export default function RegisterPage() {
       ),
     }),
     onSubmit: (value) => {
-      console.log('value ===', value);
+      //   console.log('value ===', value);
       //   if (value.password === value.password2) {
       handleRegister(value);
       //   }
@@ -64,66 +67,76 @@ export default function RegisterPage() {
   function handleRegister(userCredentialsObj) {
     axios
       .post('https://reqres.in/api/register', userCredentialsObj)
-      .then((ats) => console.log('ats ===', ats))
+      .then((ats) => {
+        console.log('Sveiki sugryze :', userCredentialsObj.email);
+        setLoginStatus('success');
+        // console.log('loginStatus ===', loginStatus);
+      })
       .catch((error) => {
         // prisiloginti nepavyko
-        console.warn('ivyko klaida:', error);
-        console.log('error.response.data.error ===', error.response.data.error);
+        console.log(error.response.data.error);
         // formik.errors.email = error.response.data.error;
         // formik.setErrors({ email: error.response.data.error });
+        setLoginStatus('failed');
+        // console.log('loginStatus ===', loginStatus);
         formik.setErrors({ email: 'Email or password not found' });
       });
   }
 
+  //   const ctx = useContext(AuthContext);
   return (
     <Wrap>
-      <Form onSubmit={formik.handleSubmit}>
-        <Input
-          type='text'
-          placeholder='Email'
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          id='email'
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <ErrorMsg>{formik.errors.email}</ErrorMsg>
-        )}
-        <Input
-          type='number'
-          placeholder='Age'
-          onChange={formik.handleChange}
-          value={formik.values.age}
-          id='age'
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.age && formik.errors.age && (
-          <ErrorMsg>{formik.errors.age}</ErrorMsg>
-        )}
-        <Input
-          type='password'
-          placeholder='Password'
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          id='password'
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.password && formik.errors.password && (
-          <ErrorMsg>{formik.errors.password}</ErrorMsg>
-        )}
-        <Input
-          type='password'
-          placeholder='Repeat Password'
-          onChange={formik.handleChange}
-          value={formik.values.password2}
-          id='password2'
-          onBlur={formik.handleBlur}
-        />
-        {formik.errors.password2 && formik.touched.password2 && (
-          <ErrorMsg>{formik.errors.password2}</ErrorMsg>
-        )}
-        <button type='submit'>Register</button>
-      </Form>
+      {loginStatus === 'success' && <h2>Sveikiname, Jums pavyko</h2>}
+      {loginStatus === 'failed' && <h2>Bandykite dar karta</h2>}
+      {loginStatus !== 'success' && (
+        <Form onSubmit={formik.handleSubmit}>
+          <Input
+            type='text'
+            placeholder='Email'
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            id='email'
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <ErrorMsg>{formik.errors.email}</ErrorMsg>
+          )}
+          <Input
+            type='number'
+            placeholder='Age'
+            onChange={formik.handleChange}
+            value={formik.values.age}
+            id='age'
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.age && formik.errors.age && (
+            <ErrorMsg>{formik.errors.age}</ErrorMsg>
+          )}
+          <Input
+            type='password'
+            placeholder='Password'
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            id='password'
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.password && formik.errors.password && (
+            <ErrorMsg>{formik.errors.password}</ErrorMsg>
+          )}
+          <Input
+            type='password'
+            placeholder='Repeat Password'
+            onChange={formik.handleChange}
+            value={formik.values.password2}
+            id='password2'
+            onBlur={formik.handleBlur}
+          />
+          {formik.errors.password2 && formik.touched.password2 && (
+            <ErrorMsg>{formik.errors.password2}</ErrorMsg>
+          )}
+          <button type='submit'>Register</button>
+        </Form>
+      )}
     </Wrap>
   );
 }
